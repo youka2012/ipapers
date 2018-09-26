998719cdb8384d73d72515782897729925c78736
youka2016 <chaomar@qq.com>
2018-09-26 19:08:40 +0800

    1

--- client/src/components/papers/PaperCreate.vue
+++ client/src/components/papers/PaperCreate.vue
@@ -229,7 +229,7 @@
         methods: {
             addQuestion() {
                 this.$refs["questionForm"].validate(valid => {
-                    if (valid && this.newQuestion !== 'INPUT' && !this.newQuestion.items.some(item => item.content === '')) {
+                    if (valid && this.newQuestion.type !== 'INPUT' && !this.newQuestion.items.some(item => item.content === '')) {
                         this.$Message.success("验证通过!");
                         this.paper.questions.push(
                             JSON.parse(JSON.stringify(this.newQuestion))
